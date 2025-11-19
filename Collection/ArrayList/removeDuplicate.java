6d6ad90040062e1965efe1d9447a0c8da7b0145f
ivoyantTraining <arpithas1920@gmail.com>
2025-11-20 00:25:19 +0530

    removeDuplicate program added

---
+++ Collection/ArrayList/removeDuplicate.java
@@ -0,0 +1,15 @@
+package JavaCourse.Collection.ArrayList;
+
+import java.util.*;
+
+public class removeDuplicate {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 2, 3, 4, 4, 5);
+
+        Set<Integer> uniqueNumbers = new LinkedHashSet<>(numbers);
+
+        List<Integer> result = new ArrayList<>(uniqueNumbers);
+
+        System.out.println("List after removing element"+ result);
+    }
+}
